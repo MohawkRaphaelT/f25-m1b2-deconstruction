3e591546453f49fa78f94be0af34c0cba6a41c01
RaphaelT <raphael.tetreault@mohawkcollege.ca>
2025-09-16 18:07:06 -0400

    Add sample code

--- activity/Program.cs
+++ activity/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int swordCost = 25;
+
+// Step 1: Ask player for name, remember it
+Console.WriteLine("Traveller, what is your name?");
+string playerName = Console.ReadLine();
+
+// Step 2: Ask player for sum of held gold (currency)
+Console.WriteLine("How much gold do you have with you?");
+int playerGold = int.Parse( Console.ReadLine() );
+
+// Step 3: tell player about sword
+Console.WriteLine($"Fabulous. We are selling a bronze sword for {swordCost} gold.");
+
+// Step4: buy item if able, deduct cost from gold
+if (playerGold >= swordCost)
+{
+    playerGold -= swordCost;
+    Console.WriteLine($"Thank you for you purchase! Remaining gold: {playerGold}");
+}
+else // if not the case
+{
+    Console.WriteLine($"{playerName}, you do not have enough gold!");
+    Console.WriteLine($"The sword costs {swordCost}, you only have {playerGold} gold!");
+}
